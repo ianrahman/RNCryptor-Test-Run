d86d1aec3bc68e5458a46467bab899f27a239a29
ianrahman <ian.alexander.rahman@gmail.com>
2016-04-16 15:48:01 -0400

    Created workspace and added RNCryptor Pod

---
+++ RNCryptor Test Run/RNCryptor Test Run/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RNCryptor Test Run
+//
+//  Created by Ian Alexander Rahman on 4/16/16.
+//  Copyright © 2016 Ian Alexander Rahman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ RNCryptor Test Run/RNCryptor Test Run/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RNCryptor Test Run
+//
+//  Created by Ian Alexander Rahman on 4/16/16.
+//  Copyright © 2016 Ian Alexander Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
