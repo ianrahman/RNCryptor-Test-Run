6ec0b4d093ed93e872028bf1e641fa163cc7a8b2
ianrahman <ian.alexander.rahman@gmail.com>
2016-04-23 11:05:17 -0400

    Added views programmatically

--- RNCryptor Test Run/ViewController.m
+++ RNCryptor Test Run/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "RNCryptor/RNCryptor.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) UITextField *password;
+@property (strong, nonatomic) UITextField *plaintext;
+@property (strong, nonatomic) UITextField *ciphertext;
+@property (strong, nonatomic) UIButton *convertButton;
 
 @end
 
@@ -16,12 +22,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    // Set the placeholder text of the text fields and convert button
+    self.password.text = @"Input your password here";
+    self.plaintext.text = @"Input the text to be encrypted here";
+    self.convertButton.titleLabel.text = @"Convert";
+    
+    // Add the text field views and convert button to the main view
+    [self.view addSubview:self.password];
+    [self.view addSubview:self.plaintext];
+    [self.view addSubview:self.ciphertext];
+    [self.view addSubview:self.convertButton];
+    
+    // Turn off taslation of autoresizing masks into constraints
+    [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    // Set constraings for text fields
+    [self.password.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [self.password.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:30].active = YES;
+    [self.password.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.8].active = YES;
+    [self.plaintext.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [self.plaintext.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.8].active = YES;
+    [self.plaintext.topAnchor constraintEqualToAnchor:self.password.bottomAnchor constant:20].active = YES;
+    [self.plaintext.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.3].active = YES;
+    [self.ciphertext.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [self.ciphertext.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.8].active = YES;
+    [self.ciphertext.topAnchor constraintEqualToAnchor:self.plaintext.bottomAnchor constant:20].active = YES;
+    [self.ciphertext.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.3].active = YES;
+    
+    // Set constraints for convert button
+    [self.convertButton.topAnchor constraintEqualToAnchor:self.ciphertext.bottomAnchor constant:10].active = YES;
+    [self.convertButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    
 }
 
 @end
